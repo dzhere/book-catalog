cdfe7e28ab50bfe9b1c346ce56779b0ecfa02c31
Mykhailo <dzgamech@gmail.com>
2020-12-16 16:51:31 +0200

    Initial commit

---
+++ core/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django_countries.fields import CountryField
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=35)
+    last_name = models.CharField(max_length=35)
+    birtday_date = models.DateField()
+    country = CountryField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Genre(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    authors = models.ManyToManyField(Author)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    date = models.DateField()
+    country = CountryField()
+
+    def __str__(self):
+        return f'{self.authors}: {self.title}'
